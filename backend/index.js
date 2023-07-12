a914a66a3c5fb9254fd1fed0d630410fdaadacee
DhaanuI <dhaanu97@gmail.com>
2023-07-12 10:33:00 +0530

    improvised prompts

--- backend/index.js
+++ backend/index.js
@@ -6,26 +6,46 @@ const cors = require("cors")
 app.use(cors())
 app.use(express.json())
 
-const configuration = new Configuration({
-    apiKey: process.env.GPT_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+
+async function generateCompletion(prompt) {
+    try {
+        const configuration = new Configuration({
+            apiKey: process.env.GPT_API_KEY,
+        });
+
+        const openai = new OpenAIApi(configuration);
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            max_tokens: 500,
+            n: 1
+        });
+
+        const { choices } = response.data;
+        if (choices && choices.length > 0) {
+            const completion = choices[0].text.trim();
+            return completion;
+        } else {
+            return false;
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
 
 app.get("/", (req, res) => {
     res.send("Homepage")
 })
 
 app.post('/process', async (req, res) => {
+    const { question } = req.body
     try {
-
-        const response = await openai.createCompletion({
-            model: "text-davinci-002",
-            prompt: req.body.text,
-            max_tokens: 7,
-            temperature: 0,
-        });
-        const result = response.data.choices[0].text == undefined ? "This is CHATGPT generated" : response.data.choices[0].text.trim().replace(/\n/g, '');
-        res.send({ "message": result })
+        prompt = question;
+        const response = await generateCompletion(prompt);
+        console.log(response)
+        res.json({ response });
     }
     catch (err) {
         res.send({ "error": err })
